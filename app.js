c9aa65709d5d813a68909e1c60d43e937e0aed3c
unknown <andresfelipehc@ufps.edu.co>
2022-04-23 16:45:27 -0500

    Se crea una lista estatica como prueba, y se crea funcionalidad de opciones, usando stdin y stdout, y se hacer funcionalidad de hacer repetitiva hasta que la opcion sea 0

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 require('colors');
+const { mostrarMenu, pausa } = require('./helpers/mensajes');
+
 
 
 const main = async()=>{
     console.clear();
-    console.log('Prueba'.red);
+
+    let opt = '';
+
+    do {
+       opt = await mostrarMenu();
+       if(opt !== '0') await pausa();
+    } while (opt != '0');
+    
 }
 
 main();
