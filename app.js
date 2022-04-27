bc71203a6aedd6b21536227dbf153b0fd507dd98
unknown <andresfelipehc@ufps.edu.co>
2022-04-27 13:01:13 -0500

    Agregando funcionalidad de guardar en un json los datos y leerlos para crear lista de tareas

--- app.js
+++ app.js
@@ -1,38 +1,44 @@
-const { inquirerMenu, 
-        pausa,
-        leerInput } = require('./helpers/inquirer');
-const Tareas = require('./models/tareas');
-
 require('colors');
 
+const { guardarDB, leerDB } = require('./helpers/guardarArchivo');
+const { inquirerMenu,
+    pausa,
+    leerInput } = require('./helpers/inquirer');
+const Tareas = require('./models/tareas');
 
 
-
-const main = async()=>{
-    console.clear();
+const main = async () => {
 
     let opt = '';
     const tareas = new Tareas();
 
-    do {
-       opt = await inquirerMenu();
-       console.log({opt})
+    const tareaDB = leerDB();
 
-       switch (opt) {
-           case '1':
-               const desc = await leerInput('Descripcion:');
-               tareas.crearTarea(desc)
-               break;
-       
-           case '2':
-               console.log(tareas._listado);
-               break;
-       }
-       
-       if(opt !== '0') await pausa();
-       
+    if (tareaDB) {//Verifica si hay tareas en base de datos y las carga al listado
+        tareas.cargarTareasFromArray(tareaDB);
+    }
+
+    do {
+        opt = await inquirerMenu();
+        console.log({ opt })
+
+        switch (opt) {
+            case '1':
+                const desc = await leerInput('Descripcion:');
+                tareas.crearTarea(desc)
+                break;
+
+            case '2':
+                tareas.listadoCompleto();
+                break;
+        }
+
+        guardarDB(tareas.listadoArr);
+
+        if (opt !== '0') await pausa();
+
     } while (opt != '0');
-    
+
 }
 
 main();
--- models/tareas.js
+++ models/tareas.js
@@ -1,16 +1,56 @@
 const Tarea = require("./tarea");
 
-class Tareas {
-    _listado = { };
+require('colors');
 
-    constructor(){
+class Tareas {
+    _listado = {};
+
+    //getter
+    get listadoArr() {
+        const listado = [];
+
+        //Extrae de _listado el id -> despues recorre ese listado con el foreach(id)
+        //y lo guarda en una tarea
+        //para despues insertarla en el arreglo listado
+        Object.keys(this._listado).forEach(key => {
+            const tarea = this._listado[key];
+            listado.push(tarea);
+        });
+
+        return listado;
+    }
+
+    constructor() {
         this._listado = {};
     }
 
-    crearTarea(desc=''){
+    cargarTareasFromArray(tareas = []) {
+        tareas.forEach(tarea => {
+            this._listado[tarea.id] = tarea;
+        });
+
+    }
+
+    crearTarea(desc = '') {
         const tarea = new Tarea(desc);
         this._listado[tarea.id] = tarea;
     }
+
+    listadoCompleto() {
+
+        this.listadoArr.forEach((tarea, i) => {
+            const idx = `${i + 1}`.green;
+            const { desc, completadoEn } = tarea;
+            const estado =  (completadoEn) 
+                ? 'Completada'.green
+                : 'Pendiente'.red;
+
+            console.log(`${idx}. ${desc} :: ${estado}`);
+        })
+    }
 }
+
+
+
 
 module.exports = Tareas;
